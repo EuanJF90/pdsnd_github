e469e71b8bd3a3e0dc5eea5898c2058e8e9ddcd6
Euan <euanfaull@hotmail.com>
2019-07-12 18:39:20 +0100

    Refactor: Remove Unecessary Lines

--- bikeshare.py
+++ bikeshare.py
@@ -27,9 +27,7 @@ def get_filters():
     Returns:
 
     (str) city - name of the city to analyze
-
     (str) month - name of the month to filter by, or "all" to apply no month filter
-
     (str) day - name of the day of week to filter by, or "all" to apply no day filter
 
     """
